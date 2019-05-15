23981de187e20f88708b3ac02326d808f47fd78c
Baptiste Bech <bech.0204@apside-groupe.com>
2019-05-15 18:08:15 +0200

    Add test route

--- kaaBotApp.js
+++ kaaBotApp.js
@@ -11,6 +11,7 @@ let httpApi = express();
 httpApi.use(bodyParser.json());
 httpApi.use(bodyParser.urlencoded({extended: true}));
 httpApi.post('/', handleChanlege);
+httpApi.get('/test', handleTest);
 httpApi.listen(24605);
 
 
@@ -25,6 +26,18 @@ async function handleChanlege(req, res) {
     let result = '';
     try {
         result = req.body.challenge;
+    } catch(e) {
+        result = e;
+    } finally {
+        res.status(200).send(result);
+    }
+}
+
+
+async function handleTest(req, res) {
+    let result = '';
+    try {
+        result = 'Serveur UP';
     } catch(e) {
         result = e;
     } finally {
