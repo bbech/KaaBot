054dfa28811bdfdaa987ab6f203e6d6c65a13312
Baptiste Bech <bech.0204@apside-groupe.com>
2019-05-15 17:48:50 +0200

    Add challenge endpoitn for Slack

---
+++ kaaBotApp.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const slack = require('@slack/client');
+
+
+// const bot_token = 'TO_Replace';
+// const rtm = new slack.RtmClient(bot_token);
+
+
+let httpApi = express();
+httpApi.use(bodyParser.json());
+httpApi.use(bodyParser.urlencoded({extended: true}));
+httpApi.post('/', handleChanlege);
+httpApi.listen(24605);
+
+
+
+// rtm.on(slack.CLIENT_EVENTS.RTM.AUTHENTICATED, (data) => {
+//     console.log(`Authenticated to ${data.team.name}`);
+// });
+// rtm.start();
+
+
+async function handleChanlege(req, res) {
+    let result = '';
+    try {
+        result = req.body.challenge;
+    } catch(e) {
+        result = e;
+    } finally {
+        res.status(200).send(result);
+    }
+}
